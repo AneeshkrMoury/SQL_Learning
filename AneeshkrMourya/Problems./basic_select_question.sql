8794a57723633d1567edaa32ccd82e2d7d6049a5
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-09-02 15:17:12 +0530

    Add basic_select_question.sql file

--- AneeshkrMourya/Problems./basic_select_question.sql
+++ AneeshkrMourya/Problems./basic_select_question.sql
@@ -68,3 +68,20 @@ AND CITY NOT LIKE "i%"
 AND CITY NOT LIKE "o%" 
 AND CITY NOT LIKE "u%");
 
+--12 Query the Name of any student in STUDENTS who scored higher than  75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID
+SELECT Name 
+FROM STUDENTS 
+WHERE Marks > 75 
+ORDER BY RIGHT(Name, 3) ASC, ID ASC; -- remember sometime different sql platofrm may do not support right in case u can try SUBSTR, SUBSTRINGS 
+
+--13 Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT name FROM Employee
+ORDER BY name ASC
+;
+
+--14 	Submissions	Leaderboard	Discussions Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT name FROM Employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC
+;
+
